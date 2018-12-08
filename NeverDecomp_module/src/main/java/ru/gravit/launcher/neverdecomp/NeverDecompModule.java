b956ffe0479d6b7b8c28c6f16864e516f45b307d
zaxar163 <zahar.vcherachny@yandex.ru>
2018-12-08 11:07:41 +0300

    AntiDecomp.

--- NeverDecomp_module/src/main/java/ru/gravit/launcher/neverdecomp/NeverDecompModule.java
+++ NeverDecomp_module/src/main/java/ru/gravit/launcher/neverdecomp/NeverDecompModule.java
@@ -2,14 +2,9 @@ package ru.gravit.launcher.neverdecomp;
 
 import ru.gravit.utils.Version;
 
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
-
 import ru.gravit.launcher.modules.Module;
 import ru.gravit.launcher.modules.ModuleContext;
-import ru.gravit.launcher.neverdecomp.asm.AntiDecompileClassVisitor;
-import ru.gravit.launcher.serialize.config.entry.BooleanConfigEntry;
-import ru.gravit.launchserver.asm.SafeClassWriter;
+import ru.gravit.launchserver.asm.AntiDecomp;
 import ru.gravit.launchserver.modules.LaunchServerModuleContext;
 
 public class NeverDecompModule implements Module {
@@ -25,7 +20,7 @@ public class NeverDecompModule implements Module {
 
 	@Override
 	public Version getVersion() {
-		return new Version(1, 0, 1, 2);
+		return new Version(1, 0, 2, 0);
 	}
 
 	@Override
@@ -36,14 +31,9 @@ public class NeverDecompModule implements Module {
 	@Override
 	public void init(ModuleContext context1) {
 		if (context1.getType().equals(ModuleContext.Type.LAUNCHSERVER)) {
-			// Config may has boolean variable "hardAntiDecomp", which enables hard mode (needs -noverify to JVM)
 			LaunchServerModuleContext context = (LaunchServerModuleContext) context1;
-			final boolean hobf = context.launchServer.config.block.hasEntry("hardAntiDecomp") ? context.launchServer.config.block.getEntryValue("hardAntiDecomp", BooleanConfigEntry.class) : false;
 			context.launchServer.buildHookManager.registerClassTransformer((src, name, e) -> {
-				ClassReader classReader = new ClassReader(src);
-				ClassWriter writer = new SafeClassWriter(e.reader, ClassWriter.COMPUTE_MAXS);
-				classReader.accept(new AntiDecompileClassVisitor(writer, hobf), ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
-				return writer.toByteArray();
+				return AntiDecomp.expAntiDecomp(src, e.reader);
 			});
 		}
 	}
