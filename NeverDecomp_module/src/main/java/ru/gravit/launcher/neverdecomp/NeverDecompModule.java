c4705248d6e6576294a9ff62edd4dd263b682533
zaxar163 <zahar.vcherachny@yandex.ru>
2018-12-14 20:16:23 +0300

    Теперь ::wrap.

--- NeverDecomp_module/src/main/java/ru/gravit/launcher/neverdecomp/NeverDecompModule.java
+++ NeverDecomp_module/src/main/java/ru/gravit/launcher/neverdecomp/NeverDecompModule.java
@@ -5,6 +5,7 @@ import ru.gravit.utils.Version;
 import ru.gravit.launcher.modules.Module;
 import ru.gravit.launcher.modules.ModuleContext;
 import ru.gravit.launchserver.asm.AntiDecomp;
+import ru.gravit.launchserver.manangers.BuildHookManager;
 import ru.gravit.launchserver.modules.LaunchServerModuleContext;
 
 public class NeverDecompModule implements Module {
@@ -32,9 +33,7 @@ public class NeverDecompModule implements Module {
 	public void init(ModuleContext context1) {
 		if (context1.getType().equals(ModuleContext.Type.LAUNCHSERVER)) {
 			LaunchServerModuleContext context = (LaunchServerModuleContext) context1;
-			context.launchServer.buildHookManager.registerClassTransformer((src, name, e) -> {
-				return AntiDecomp.expAntiDecomp(src, e.reader);
-			});
+			context.launchServer.buildHookManager.registerClassTransformer(BuildHookManager.wrap(AntiDecomp::antiDecomp));
 		}
 	}
 
