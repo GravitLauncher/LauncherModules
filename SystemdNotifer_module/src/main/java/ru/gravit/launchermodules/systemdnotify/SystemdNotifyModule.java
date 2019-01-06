f87bdf0e1eeed4ca097e4ab90b99b0639aa4b52a
Gravit <gravit.min@ya.ru>
2019-01-06 23:00:58 +0700

    SystemdNotifyModule

---
+++ SystemdNotifer_module/src/main/java/ru/gravit/launchermodules/systemdnotify/SystemdNotifyModule.java
@@ -0,0 +1,57 @@
+package ru.gravit.launchermodules.systemdnotify;
+
+import ru.gravit.launcher.modules.Module;
+import ru.gravit.launcher.modules.ModuleContext;
+import ru.gravit.utils.Version;
+import ru.gravit.utils.helper.LogHelper;
+
+import java.io.IOException;
+
+public class SystemdNotifyModule implements Module {
+    public static Version version =new Version(1,0,0);
+    @Override
+    public String getName() {
+        return "SystemdNotifer";
+    }
+
+    @Override
+    public Version getVersion() {
+        return version;
+    }
+
+    @Override
+    public int getPriority() {
+        return 0;
+    }
+
+    @Override
+    public void init(ModuleContext moduleContext) {
+
+    }
+
+    @Override
+    public void postInit(ModuleContext moduleContext) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("systemd-notify", "--ready");
+        try {
+            Process process = processBuilder.start();
+            LogHelper.debug("Systemd notify successful");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void preInit(ModuleContext moduleContext) {
+
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
