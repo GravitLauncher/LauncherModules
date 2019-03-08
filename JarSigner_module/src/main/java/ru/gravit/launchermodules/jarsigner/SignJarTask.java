d4320ce29b8a142a75b86237139e92a72e00b481
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-03-08 11:09:08 +0300

    [FEATURE] Модуль JarSigner.

---
+++ JarSigner_module/src/main/java/ru/gravit/launchermodules/jarsigner/SignJarTask.java
@@ -0,0 +1,51 @@
+package ru.gravit.launchermodules.jarsigner;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.jar.JarInputStream;
+import java.util.zip.ZipEntry;
+
+import ru.gravit.launchserver.LaunchServer;
+import ru.gravit.launchserver.binary.tasks.LauncherBuildTask;
+import ru.gravit.utils.helper.IOHelper;
+
+public class SignJarTask implements LauncherBuildTask {
+
+	private final ModuleImpl impl;
+	private final LaunchServer srv;
+
+	public SignJarTask(LaunchServer srv, ModuleImpl impl) {
+		this.srv = srv;
+		this.impl = impl;
+	}
+
+	@Override
+	public String getName() {
+		return "SignJar";
+	}
+
+	@Override
+	public Path process(Path inputFile) throws IOException {
+		Path toRet = srv.launcherBinary.nextPath("signed");
+		try (SignerJar output = new SignerJar(IOHelper.newOutput(toRet),
+                SignerJar.getStore(new File(impl.config.key).toPath(), impl.config.storepass, impl.config.algo),
+                impl.config.keyalias, impl.config.pass);
+             JarInputStream input = new JarInputStream(IOHelper.newInput(inputFile))) {
+            ZipEntry e = input.getNextEntry();
+            while (e != null) {
+                output.addFileContents(e, input);
+                e = input.getNextEntry();
+            }
+            input.getManifest().getMainAttributes().forEach((a, b) -> {
+            	output.addManifestAttribute(a.toString(), b.toString());
+            });
+        }
+		return toRet;
+	}
+
+	@Override
+	public boolean allowDelete() {
+		return true;
+	}
+}
