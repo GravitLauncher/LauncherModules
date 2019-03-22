073b52f785527e01ca548fc56b71130b41ce648e
Gravit <gravit.min@ya.ru>
2019-03-22 12:49:41 +0700

    [FIX] Фикс AutoSaveSessions

--- AutoSaveSessions_module/src/main/java/ru/gravit/launchermodules/autosavesessions/AutoSaveSessionsModule.java
+++ AutoSaveSessions_module/src/main/java/ru/gravit/launchermodules/autosavesessions/AutoSaveSessionsModule.java
@@ -63,6 +63,10 @@ public class AutoSaveSessionsModule implements Module {
             }.getType();
             try (Reader reader = IOHelper.newReader(file)) {
                 Set<Client> clientSet = LaunchServer.gson.fromJson(reader, setType);
+                for(Client client : clientSet)
+                {
+                    if(client.isAuth) client.updateAuth();
+                }
                 context.launchServer.sessionManager.loadSessions(clientSet);
                 LogHelper.info("Loaded %d sessions", clientSet.size());
             } catch (IOException e) {
