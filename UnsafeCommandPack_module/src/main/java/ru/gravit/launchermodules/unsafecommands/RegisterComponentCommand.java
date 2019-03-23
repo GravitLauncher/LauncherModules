0e32128c6c8f569d67eff4e94777c6b8ef696345
Gravit <gravit.min@ya.ru>
2019-03-23 11:27:09 +0700

    [FEATURE] UnsafeCommands module

---
+++ UnsafeCommandPack_module/src/main/java/ru/gravit/launchermodules/unsafecommands/RegisterComponentCommand.java
@@ -0,0 +1,30 @@
+package ru.gravit.launchermodules.unsafecommands;
+
+import ru.gravit.launchserver.LaunchServer;
+import ru.gravit.launchserver.command.Command;
+import ru.gravit.launchserver.components.Component;
+
+public class RegisterComponentCommand extends Command {
+    public RegisterComponentCommand(LaunchServer server) {
+        super(server);
+    }
+
+    @Override
+    public String getArgsDescription() {
+        return "[name] [classname]";
+    }
+
+    @Override
+    public String getUsageDescription() {
+        return "register custom component";
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void invoke(String... args) throws Exception {
+        verifyArgs(args, 2);
+        Class clazz = Class.forName(args[1]);
+        if(clazz == null) throw new ClassNotFoundException(args[1]);
+        Component.registerComponent(args[0], clazz);
+    }
+}
