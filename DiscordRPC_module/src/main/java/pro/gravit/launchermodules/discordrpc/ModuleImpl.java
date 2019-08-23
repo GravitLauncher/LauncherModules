bb42a813e6f389366fd0e16b178abb954f7efbb1
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-08-23 18:05:49 +0300

    [FIX] Code-duplicate.

--- DiscordRPC_module/src/main/java/pro/gravit/launchermodules/discordrpc/ModuleImpl.java
+++ DiscordRPC_module/src/main/java/pro/gravit/launchermodules/discordrpc/ModuleImpl.java
@@ -55,33 +55,28 @@ public class ModuleImpl implements Module {
     @Override
     public void postInit(ModuleContext context1) {
     	LaunchServerModuleContext context = (LaunchServerModuleContext) context1;
-    	try {
-			context.launchServer.buildHookManager.registerClientModuleClass("pro.gravit.launchermodules.discordrpc.ClientModule");
-			context.launchServer.buildHookManager.registerHook(ctx -> {
-				try (ZipInputStream in = IOHelper.newZipInput(IOHelper.getCodeSource(ModuleImpl.class))) {
-					ctx.data.reader.getCp().add(new JarFile(IOHelper.getCodeSource(ModuleImpl.class).toFile()));
-			        ctx.output.putNextEntry(IOHelper.newZipEntry("rpc.config.json"));
-			        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			        Writer w = new OutputStreamWriter(baos, IOHelper.UNICODE_CHARSET);
-			        Config.getOrCreate(config).write(w);
-			        w.flush();
-			        ctx.output.write(baos.toByteArray());
-			        JarHelper.zipWalk(in, (i, e) -> {
-			        	if (!e.getName().startsWith("META-INF") && !e.getName().startsWith(keepClass)) {
-			        		ctx.output.putNextEntry(IOHelper.newZipEntry(e));
-			        		IOHelper.transfer(i, ctx.output);
-					        ctx.fileList.add(e.getName());
-			        	}
-			        });
-			        ctx.fileList.add("rpc.config.json");
-				} catch (IOException e) {
-					LogHelper.error(e);
-				}
-			});
-			context.launchServer.buildHookManager.registerIncludeClass("pro.gravit.launchermodules.discordrpc.ClientModule", IOHelper.read(ModuleImpl.class.getResourceAsStream("pro.gravit.launchermodules.discordrpc.ClientModule".replace('.', '/') + ".class")));
-    	} catch (IOException e) {
-			LogHelper.error(e);
-		}
+		context.launchServer.buildHookManager.registerClientModuleClass("pro.gravit.launchermodules.discordrpc.ClientModule");
+		context.launchServer.buildHookManager.registerHook(ctx -> {
+			try (ZipInputStream in = IOHelper.newZipInput(IOHelper.getCodeSource(ModuleImpl.class))) {
+				ctx.data.reader.getCp().add(new JarFile(IOHelper.getCodeSource(ModuleImpl.class).toFile()));
+		        ctx.output.putNextEntry(IOHelper.newZipEntry("rpc.config.json"));
+		        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		        Writer w = new OutputStreamWriter(baos, IOHelper.UNICODE_CHARSET);
+		        Config.getOrCreate(config).write(w);
+		        w.flush();
+		        ctx.output.write(baos.toByteArray());
+		        JarHelper.zipWalk(in, (i, e) -> {
+		        	if (!e.getName().startsWith("META-INF") && !e.getName().startsWith(keepClass)) {
+		        		ctx.output.putNextEntry(IOHelper.newZipEntry(e));
+		        		IOHelper.transfer(i, ctx.output);
+				        ctx.fileList.add(e.getName());
+		        	}
+		        });
+		        ctx.fileList.add("rpc.config.json");
+			} catch (IOException e) {
+				LogHelper.error(e);
+			}
+		});
         config = context1.getModulesConfigManager().getModuleConfig(getName());
     }
 
