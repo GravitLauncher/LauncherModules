d6a7e46ed889e8b02d09840fb7f69b0159dc07e7
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-09-17 07:25:07 +0200

    [FEATURE] LauncherStatyScreen module (1/2).

---
+++ LauncherStartScreen_lmodule/src/main/java/pro/gravit/launchermodules/startscreen/ModuleImpl.java
@@ -0,0 +1,63 @@
+package pro.gravit.launchermodules.startscreen;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Path;
+
+import pro.gravit.launcher.Launcher;
+import pro.gravit.launcher.modules.LauncherInitContext;
+import pro.gravit.launcher.modules.LauncherModule;
+import pro.gravit.launcher.modules.LauncherModuleInfo;
+import pro.gravit.launchserver.modules.events.LaunchServerPostInitPhase;
+import pro.gravit.utils.Version;
+import pro.gravit.utils.helper.IOHelper;
+import pro.gravit.utils.helper.LogHelper;
+
+public class ModuleImpl extends LauncherModule {
+	public ModuleImpl() {
+		super(new LauncherModuleInfo("LauncherStartScreen", version, Integer.MAX_VALUE-200, new String[0]));
+	}
+	
+    public static final Version version = new Version(0, 1, 0, 0, Version.Type.LTS);
+
+    public Path configFile = null;
+    public Config config = null;
+
+	@Override
+	public void init(LauncherInitContext initContext) {
+        registerEvent(this::finish, LaunchServerPostInitPhase.class);
+	}
+	public void finish(LaunchServerPostInitPhase context) {
+        configFile = context.server.modulesManager.getConfigManager().getModuleConfig("jar-signing");
+        if (IOHelper.exists(configFile)) {
+            try (Reader reader = IOHelper.newReader(configFile)) {
+                config = Launcher.gsonManager.configGson.fromJson(reader, Config.class);
+            } catch (IOException e) {
+                LogHelper.error(e);
+            }
+        } else {
+            LogHelper.debug("Create new jar signing config file");
+            try (Writer writer = IOHelper.newWriter(configFile)) {
+                config = new Config();
+                Launcher.gsonManager.configGson.toJson(config, writer);
+            } catch (IOException e) {
+                LogHelper.error(e);
+            }
+        }
+        context.server.launcherBinary.tasks.add(new SignJarTask(context.server, this));
+    }
+
+
+    public void reload() {
+        try (Reader reader = IOHelper.newReader(configFile)) {
+            config = Launcher.gsonManager.configGson.fromJson(reader, Config.class);
+        } catch (IOException e) {
+            LogHelper.error(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.err.println("This is module, use with GravitLauncher`s LaunchServer.");
+    }
+}
