143a6ca115cada25a942b0dacc83d59c76595421
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-11-29 18:38:24 +0100

    [FIX] Получение конфигов через ModulesConfigManager.

--- DiscordRPC_module/src/main/java/pro/gravit/launchermodules/discordrpc/ModuleImpl.java
+++ DiscordRPC_module/src/main/java/pro/gravit/launchermodules/discordrpc/ModuleImpl.java
@@ -4,7 +4,6 @@ import org.objectweb.asm.Type;
 import pro.gravit.launcher.modules.LauncherInitContext;
 import pro.gravit.launcher.modules.LauncherModule;
 import pro.gravit.launcher.modules.LauncherModuleInfo;
-import pro.gravit.launcher.modules.events.PreConfigPhase;
 import pro.gravit.launchserver.LaunchServer;
 import pro.gravit.launchserver.modules.events.LaunchServerPostInitPhase;
 import pro.gravit.utils.Version;
@@ -17,25 +16,22 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.jar.JarFile;
 import java.util.zip.ZipInputStream;
 
 public class ModuleImpl extends LauncherModule {
     private static final String keepClass = Type.getInternalName(ModuleImpl.class);
     public static final Version version = new Version(1, 1, 0, 0, Version.Type.LTS);
-    public Path config = Paths.get("config/DiscordRPC/DiscordRPCConfig.json");
+    public Path config;
 
     public ModuleImpl() {
         super(new LauncherModuleInfo("DiscordRPC", version));
     }
 
-    public void preInit(PreConfigPhase phase) {
-        Config.getOrCreate(config);
-    }
-
     public void postInit(LaunchServerPostInitPhase phase) {
         LaunchServer context = phase.server;
+    	config = context.modulesManager.getConfigManager().getModuleConfig(this.getModuleInfo().name, "DiscordRPCConfig");
+    	Config.getOrCreate(config);
         context.buildHookManager.registerClientModuleClass("pro.gravit.launchermodules.discordrpc.ClientModule");
         context.buildHookManager.registerHook(ctx -> {
             try (ZipInputStream in = IOHelper.newZipInput(IOHelper.getCodeSource(ModuleImpl.class))) {
@@ -67,7 +63,6 @@ public class ModuleImpl extends LauncherModule {
 
     @Override
     public void init(LauncherInitContext launcherInitContext) {
-        registerEvent(this::preInit, PreConfigPhase.class);
         registerEvent(this::postInit, LaunchServerPostInitPhase.class);
     }
 }
