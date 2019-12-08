9ee94152da6ae8e3ea045484f60b2a2246b4e993
Gravit <gravit.min@ya.ru>
2019-12-09 00:56:27 +0700

    [FIX] FIndSystemPatcher

--- UnsafeCommandPack_module/src/main/java/pro/gravit/launchermodules/unsafecommands/patcher/impl/FindSystemPatcher.java
+++ UnsafeCommandPack_module/src/main/java/pro/gravit/launchermodules/unsafecommands/patcher/impl/FindSystemPatcher.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class FindSystemPatcher extends ClassTransformerPatcher {
     public static List<String> noTriggeredMethods = new ArrayList<>();
+    public static List<String> noTriggeredMethodsCL = new ArrayList<>();
     @Override
     public ClassVisitor getVisitor(ClassReader reader, ClassWriter cw) {
         return new ClassVisitor(Opcodes.ASM5) {
@@ -17,7 +18,11 @@ public class FindSystemPatcher extends ClassTransformerPatcher {
                 return new MethodVisitor(Opcodes.ASM5) {
                     @Override
                     public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
-                        if(opcode == Opcodes.INVOKESTATIC &&  "java/lang/System".equals(owner) && !noTriggeredMethods.contains(name)) {
+                        if(opcode == Opcodes.INVOKESTATIC && ( ("java/lang/System".equals(owner) && !noTriggeredMethods.contains(name)) || ("java/lang/ClassLoader".equals(owner) && !noTriggeredMethodsCL.contains(name)))) {
+                            LogHelper.info("Class %s method %s call %s.%s(%s)", reader.getClassName(), methodName, owner, name, descriptor);
+                        }
+                        if("defineClass".equals(name))
+                        {
                             LogHelper.info("Class %s method %s call %s.%s(%s)", reader.getClassName(), methodName, owner, name, descriptor);
                         }
                         super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
@@ -30,5 +35,11 @@ public class FindSystemPatcher extends ClassTransformerPatcher {
         noTriggeredMethods.add("currentTimeMillis");
         noTriggeredMethods.add("getProperty");
         noTriggeredMethods.add("arraycopy");
+        noTriggeredMethods.add("identityHashCode");
+        noTriggeredMethods.add("nanoTime");
+
+        noTriggeredMethodsCL.add("getSystemResource");
+        noTriggeredMethodsCL.add("getSystemResourceAsStream");
+        noTriggeredMethodsCL.add("getSystemResources");
     }
 }
