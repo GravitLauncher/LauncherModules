780fd10cdfbf257d33ca02371a5c840229d70d78
Gravita <gravita@gravit.pro>
2021-05-10 15:53:39 +0700

    [FEATURE] LogAppender

--- RemoteControl_module/src/main/java/pro/gravit/launchermodules/remotecontrol/RemoteControlWebSeverlet.java
+++ RemoteControl_module/src/main/java/pro/gravit/launchermodules/remotecontrol/RemoteControlWebSeverlet.java
@@ -4,18 +4,24 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LogEvent;
 import pro.gravit.launchserver.LaunchServer;
+import pro.gravit.launchserver.config.log4j.LogAppender;
 import pro.gravit.launchserver.socket.NettyConnectContext;
 import pro.gravit.launchserver.socket.handlers.NettyWebAPIHandler;
-import pro.gravit.utils.helper.LogHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class RemoteControlWebSeverlet implements NettyWebAPIHandler.SimpleSeverletHandler {
     private final RemoteControlModule module;
     private final LaunchServer server;
+    private transient final Logger logger = LogManager.getLogger();
 
     public RemoteControlWebSeverlet(RemoteControlModule module, LaunchServer server) {
         this.module = module;
@@ -65,23 +71,21 @@ public class RemoteControlWebSeverlet implements NettyWebAPIHandler.SimpleSeverl
                 return;
             }
         }
-        LogHelper.info("[RemoteControl][Web] IP %s execute command '%s' with token %s...", context.ip, command, accessToken.substring(0, 5));
+        logger.info("[RemoteControl][Web] IP {} execute command '{}' with token {}...", context.ip, command, accessToken.substring(0, 5));
         boolean needLog = params.get("log") != null && Boolean.parseBoolean(params.get("log"));
         LogLinesContainer container = null;
-        LogHelper.OutputEnity entity = null;
         if (needLog) {
             container = new LogLinesContainer();
-            entity = new LogHelper.OutputEnity(container, LogHelper.OutputTypes.PLAIN);
-            LogHelper.addOutput(entity);
+            LogAppender.getInstance().addListener(container);
         }
         String exception = null;
         try {
             server.commandHandler.evalNative(command, false);
         } catch (Throwable e) {
             if (needLog) {
-                LogHelper.removeOutput(entity);
+                LogAppender.getInstance().removeListener(container);
             }
-            LogHelper.error(e);
+            logger.error(e);
             exception = e.toString();
         }
         SuccessCommandResponse response = new SuccessCommandResponse();
@@ -89,7 +93,7 @@ public class RemoteControlWebSeverlet implements NettyWebAPIHandler.SimpleSeverl
         response.lines = container == null ? null : container.lines;
         response.success = exception == null;
         if (needLog) {
-            LogHelper.removeOutput(entity);
+            LogAppender.getInstance().removeListener(container);
         }
         sendHttpResponse(ctx, simpleJsonResponse(HttpResponseStatus.OK, new RemoteControlResponse<>(response)));
     }
@@ -107,17 +111,33 @@ public class RemoteControlWebSeverlet implements NettyWebAPIHandler.SimpleSeverl
         }
     }
 
-    public static class LogLinesContainer implements LogHelper.Output {
-        public List<String> lines = new ArrayList<>();
+    public static class LogLinesContainer implements Consumer<LogEvent> {
+        public List<LogEventView> lines = new ArrayList<>();
 
         @Override
-        public void println(String message) {
-            lines.add(message);
+        public void accept(LogEvent logEvent) {
+            lines.add(new LogEventView(logEvent));
+        }
+    }
+
+    public static class LogEventView {
+        public final String level;
+        public final String message;
+        public final String exception;
+        public LogEventView(LogEvent event) {
+            level = event.getLevel().toString();
+            message = event.getMessage().getFormattedMessage();
+            Throwable throwable = event.getMessage().getThrowable();
+            if(throwable != null) {
+                exception = String.format("%s: %s", throwable.getClass().getName(), throwable.getMessage());
+            } else {
+                exception = null;
+            }
         }
     }
 
     public static class SuccessCommandResponse {
-        public List<String> lines;
+        public List<LogEventView> lines;
         public String exception;
         public boolean success;
     }
