e8bc82be4d0fb86041fe464cad32314430552386
Gravita <gravita@gravit.pro>
2021-06-18 17:00:42 +0700

    [FIX] FileAuthSystem install init

--- FileAuthSystem_module/src/main/java/pro/gravit/launchermodules/fileauthsystem/commands/InstallCommand.java
+++ FileAuthSystem_module/src/main/java/pro/gravit/launchermodules/fileauthsystem/commands/InstallCommand.java
@@ -40,7 +40,9 @@ public class InstallCommand extends Command {
         }
         boolean changed = false;
         if (!(pair.core instanceof FileSystemAuthCoreProvider)) {
+            if (pair.core != null) pair.core.close();
             pair.core = new FileSystemAuthCoreProvider();
+            pair.core.init(server);
             pair.provider = null;
             pair.handler = null;
             logger.info("FileSystemAuthCoreProvider installed");
