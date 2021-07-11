5c17323bd4dc5c7f85d35fdee4f4cf4c35eb86f7
Gravita <gravita@gravit.pro>
2021-07-11 17:07:54 +0700

    [FEATURE] Downloader ProgressBar

--- UnsafeCommandPack_module/src/main/java/pro/gravit/launchermodules/unsafecommands/commands/NewDownloadAssetCommand.java
+++ UnsafeCommandPack_module/src/main/java/pro/gravit/launchermodules/unsafecommands/commands/NewDownloadAssetCommand.java
@@ -14,8 +14,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class NewDownloadAssetCommand extends Command {
     private final Logger logger = LogManager.getLogger();
@@ -48,34 +46,14 @@ public class NewDownloadAssetCommand extends Command {
         List<AsyncDownloader.SizedFile> applies = AssetDownloader.listAssets(assetDir, version);
         // Download required asset
         LogHelper.subInfo("Downloading asset, it may take some time");
-        ExecutorService e = Executors.newFixedThreadPool(4);
         long total = 0;
         for (AsyncDownloader.SizedFile file : applies) {
             total += file.size;
         }
         final long finalTotal = total;
         logger.info("Download started, total {}", finalTotal);
-        Downloader downloader = Downloader.downloadList(applies, AssetDownloader.getBase(), assetDir, new Downloader.DownloadCallback() {
-            private long lastTime = System.currentTimeMillis();
-            private long current = 0;
-            private long currentFiles = 0;
-
-            @Override
-            public void apply(long fullDiff) {
-                current += fullDiff;
-                if (System.currentTimeMillis() - lastTime > 1000) {
-                    lastTime = System.currentTimeMillis();
-                    logger.debug("Download progress {} / {} | {} / {}", current, finalTotal, currentFiles, applies.size());
-                }
-            }
-
-            @Override
-            public void onComplete(Path path) {
-                currentFiles++;
-            }
-        }, e, 4);
+        Downloader downloader = downloadWithProgressBar(applies, AssetDownloader.getBase(), assetDir);
         downloader.getFuture().get();
-        e.shutdownNow();
         // Finished
         server.syncUpdatesDir(Collections.singleton(dirName));
     }
