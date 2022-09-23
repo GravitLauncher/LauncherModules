098f88286696c7ea41205ec2ff2aa3c82bc63e3c
realkarmakun <20980281+realkarmakun@users.noreply.github.com>
2022-09-23 06:10:23 +0600

    [FEATURE] Асинхронные запросы к CF API

--- CloudflarePurge_module/src/main/java/pro/gravit/launchermodules/cfpurge/CloudflareService.java
+++ CloudflarePurge_module/src/main/java/pro/gravit/launchermodules/cfpurge/CloudflareService.java
@@ -16,17 +16,18 @@ public enum CloudflareService {
         if (cfAccess == null) {
             cfAccess = new CloudflareAccess(cloudflareApiToken);
         }
-        final var request = new CloudflareRequest(Category.PURGE_ZONE_ALL_FILES, cfAccess)
+        new CloudflareRequest(Category.PURGE_ZONE_ALL_FILES, cfAccess)
                 .identifiers(zoneId)
                 .body("purge_everything", true)
-                .asVoid();
-
-        if (request.isSuccessful()) {
-            logger.info("Successfully purged zone {}", zoneId);
-        } else {
-            logger.error("Error occurred while trying to purge zone {}\n{}",
-                    zoneId, request.getErrors());
-        }
+                .asVoidAsync()
+                .thenAccept(action -> {
+                    if (action.isSuccessful()) {
+                        logger.info("Successfully purged zone {}", zoneId);
+                    } else {
+                        logger.error("Error occurred while trying to purge zone {}\n{}",
+                                zoneId, action.getErrors());
+                    }
+                });
     }
 
     public static class Config {
