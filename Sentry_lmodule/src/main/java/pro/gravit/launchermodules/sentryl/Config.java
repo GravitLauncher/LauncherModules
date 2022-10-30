dacf8f8aa34e1be01640835b2fb9f9857fdaf040
Gravita <gravita@gravit.pro>
2022-10-30 20:51:27 +0700

    [FIX] Sentry module

--- Sentry_lmodule/src/main/java/pro/gravit/launchermodules/sentryl/Config.java
+++ Sentry_lmodule/src/main/java/pro/gravit/launchermodules/sentryl/Config.java
@@ -5,14 +5,14 @@ import pro.gravit.launcher.LauncherInject;
 public class Config {
     @LauncherInject("modules.sentry.dsn")
     public String dsn;
-    @LauncherInject("modules.sentry.captureAll")
+    @LauncherInject("modules.sentry.captureall")
     public boolean captureAll;
-    @LauncherInject("modules.sentry.setThreadExcpectionHandler")
+    @LauncherInject("modules.sentry.setthreadexcpectionhandler")
     public boolean setThreadExcpectionHandler;
 
     public static Object getDefault() {
         Config config = new Config();
         config.dsn = "YOUR_DSN";
-        return new Config();
+        return config;
     }
 }
