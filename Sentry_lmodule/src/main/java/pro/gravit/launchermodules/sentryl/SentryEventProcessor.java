4656f786658a7e4a853e5a4adf8e2c2f569cf7e3
microwin7 <usa.microwin8@gmail.com>
2023-04-27 00:03:46 +0300

    [FEATURE] Add Filtering RequestException

--- Sentry_lmodule/src/main/java/pro/gravit/launchermodules/sentryl/SentryEventProcessor.java
+++ Sentry_lmodule/src/main/java/pro/gravit/launchermodules/sentryl/SentryEventProcessor.java
@@ -3,14 +3,22 @@ package pro.gravit.launchermodules.sentryl;
 import io.sentry.EventProcessor;
 import io.sentry.Hint;
 import io.sentry.SentryEvent;
+import pro.gravit.launcher.request.RequestException;
 import pro.gravit.launchermodules.sentryl.utils.OshiUtils;
 import pro.gravit.utils.helper.JVMHelper;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class SentryEventProcessor implements EventProcessor {
     @Override
     public SentryEvent process(SentryEvent event, Hint hint) {
         if(event.getThrowable() != null && SentryModule.config.ignoreErrors != null && SentryModule.config.ignoreErrors.contains(event.getThrowable().getMessage())) {
             return null;
+        }
+        if (event.getThrowable() instanceof RequestException) {
+            event.setFingerprints(Collections.singletonList(event.getThrowable().toString()));
         }
         if(SentryModule.config.collectMemoryInfo) {
             event.getContexts().put("Memory info", OshiUtils.makeMemoryProperties());
