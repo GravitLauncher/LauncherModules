59e6e41bcb5b591a57ebdd41c9a90ec196468a87
microwin7 <usa.microwin8@gmail.com>
2023-04-26 19:22:04 +0300

    [ANY] Creating a single variable for System.getProperties()

--- Sentry_lmodule/src/main/java/pro/gravit/launchermodules/sentryl/utils/BasicProperties.java
+++ Sentry_lmodule/src/main/java/pro/gravit/launchermodules/sentryl/utils/BasicProperties.java
@@ -8,15 +8,17 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class BasicProperties {
 
     public static void setupBasicProperties(Scope scope) {
+        Properties properties = System.getProperties();
         Map<String, String> os = new HashMap<>();
-        os.put("Name", System.getProperties().getProperty("os.name"));
+        os.put("Name", properties.getProperty("os.name"));
         os.put("Arch", String.valueOf(JVMHelper.ARCH_TYPE));
         scope.setContexts("OS", os);
-        scope.setTag("OS_TYPE", System.getProperties().getProperty("os.name"));
+        scope.setTag("OS_TYPE", properties.getProperty("os.name"));
         Map<String, String> jvm = new HashMap<>();
         jvm.put("Version", String.valueOf(JVMHelper.JVM_VERSION));
         jvm.put("Bits", String.valueOf(JVMHelper.JVM_BITS));
@@ -25,21 +27,22 @@ public class BasicProperties {
         scope.setContexts("Java", jvm);
         scope.setTag("JVM_VERSION", String.valueOf(JVMHelper.JVM_VERSION));
         Map<String, String> systemProperties = new HashMap<>();
-        systemProperties.put("Name", System.getProperties().getProperty("os.name"));
-        systemProperties.put("file.encoding", System.getProperties().getProperty("file.encoding"));
-        systemProperties.put("java.class.path", System.getProperties().getProperty("java.class.path"));
-        systemProperties.put("java.class.version", System.getProperties().getProperty("java.class.version"));
-        systemProperties.put("java.endorsed.dirs", System.getProperties().getProperty("java.endorsed.dirs"));
-        systemProperties.put("java.ext.dirs", System.getProperties().getProperty("java.ext.dirs"));
-        systemProperties.put("java.home", System.getProperties().getProperty("java.home"));
-        systemProperties.put("java.io.tmpdir", System.getProperties().getProperty("java.io.tmpdir"));
-        systemProperties.put("os.arch", System.getProperties().getProperty("os.arch"));
-        systemProperties.put("sun.arch.data.model", System.getProperties().getProperty("sun.arch.data.model"));
-        systemProperties.put("sun.boot.class.path", System.getProperties().getProperty("sun.boot.class.path"));
-        systemProperties.put("sun.jnu.encoding", System.getProperties().getProperty("sun.jnu.encoding"));
-        systemProperties.put("user.language", System.getProperties().getProperty("user.language"));
-        systemProperties.put("user.timezone", System.getProperties().getProperty("user.timezone"));
-        systemProperties.put("javafx.runtime.version", System.getProperties().getProperty("javafx.runtime.version"));
+
+        systemProperties.put("Name", properties.getProperty("os.name"));
+        systemProperties.put("file.encoding", properties.getProperty("file.encoding"));
+        systemProperties.put("java.class.path", properties.getProperty("java.class.path"));
+        systemProperties.put("java.class.version", properties.getProperty("java.class.version"));
+        systemProperties.put("java.endorsed.dirs", properties.getProperty("java.endorsed.dirs"));
+        systemProperties.put("java.ext.dirs", properties.getProperty("java.ext.dirs"));
+        systemProperties.put("java.home", properties.getProperty("java.home"));
+        systemProperties.put("java.io.tmpdir", properties.getProperty("java.io.tmpdir"));
+        systemProperties.put("os.arch", properties.getProperty("os.arch"));
+        systemProperties.put("sun.arch.data.model", properties.getProperty("sun.arch.data.model"));
+        systemProperties.put("sun.boot.class.path", properties.getProperty("sun.boot.class.path"));
+        systemProperties.put("sun.jnu.encoding", properties.getProperty("sun.jnu.encoding"));
+        systemProperties.put("user.language", properties.getProperty("user.language"));
+        systemProperties.put("user.timezone", properties.getProperty("user.timezone"));
+        systemProperties.put("javafx.runtime.version", properties.getProperty("javafx.runtime.version"));
         scope.setContexts("System Properties", systemProperties);
 
         DateFormat df = new SimpleDateFormat("'Date:' " + "yyyy.dd.MM" + " 'Time:' " + "HH:mm:ss" + " 'Timezone:' X");
