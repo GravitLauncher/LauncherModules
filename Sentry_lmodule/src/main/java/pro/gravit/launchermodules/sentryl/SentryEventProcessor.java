b744173485515fc4a45bdfa4d304a703cbccf1ef
microwin7 <usa.microwin8@gmail.com>
2023-05-06 18:17:40 +0300

    Fix issue get Throwable message [2]

--- Sentry_lmodule/src/main/java/pro/gravit/launchermodules/sentryl/SentryEventProcessor.java
+++ Sentry_lmodule/src/main/java/pro/gravit/launchermodules/sentryl/SentryEventProcessor.java
@@ -8,19 +8,17 @@ import pro.gravit.launchermodules.sentryl.utils.OshiUtils;
 import pro.gravit.utils.helper.JVMHelper;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class SentryEventProcessor implements EventProcessor {
     @Override
     public SentryEvent process(SentryEvent event, Hint hint) {
-        if(event.getThrowable() != null && SentryModule.config.ignoreErrors != null && SentryModule.config.ignoreErrors.contains(event.getThrowable().getMessage())) {
+        if (event.getThrowable() != null && SentryModule.config.ignoreErrors != null && SentryModule.config.ignoreErrors.contains(event.getThrowable().getMessage())) {
             return null;
         }
         if (event.getThrowable() instanceof RequestException) {
-            event.setFingerprints(Collections.singletonList(event.getThrowable().getMessage()));
+            event.setFingerprints(Arrays.asList("RequestException: ", event.getThrowable().getMessage()));
         }
-        if(SentryModule.config.collectMemoryInfo) {
+        if (SentryModule.config.collectMemoryInfo) {
             event.getContexts().put("Memory info", OshiUtils.makeMemoryProperties());
         }
         long uptime = JVMHelper.RUNTIME_MXBEAN.getUptime();
