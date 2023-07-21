caa99dbac95d3bef71a68ae6d4e7dac6b92cab1c
microwin7 <usa.microwin8@gmail.com>
2023-07-21 10:10:14 +0300

    [FIX] Loading natives for DiscordGame

--- DiscordGame_lmodule/src/main/java/pro/gravit/launchermodules/discordgame/DiscordBridge.java
+++ DiscordGame_lmodule/src/main/java/pro/gravit/launchermodules/discordgame/DiscordBridge.java
@@ -44,9 +44,8 @@ public class DiscordBridge {
         } else {
             arch = JVMHelper.ARCH_TYPE.name;
         }
-
-        DiscordBridge.loadNative(baseDir, "discord_game_sdk_jni", osFolder, arch);
-        Core.initDiscordNative(DiscordBridge.loadNative(baseDir, "discord_game_sdk", osFolder, arch));
+        DiscordBridge.loadNative(baseDir, "discord_game_sdk", osFolder, arch);
+        Core.initDiscordNative(DiscordBridge.loadNative(baseDir, "discord_game_sdk_jni", osFolder, arch));
 
     }
 
