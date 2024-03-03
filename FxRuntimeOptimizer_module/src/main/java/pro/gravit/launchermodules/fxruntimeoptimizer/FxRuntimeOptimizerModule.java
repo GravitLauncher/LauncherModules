ade7dbc8584d2f13b68bc26dee4927b4e869f639
Gravita <12893402+gravit0@users.noreply.github.com>
2024-03-03 13:39:28 +0700

    [FEATURE] FxRuntimeOptimizerModule

---
+++ FxRuntimeOptimizer_module/src/main/java/pro/gravit/launchermodules/fxruntimeoptimizer/FxRuntimeOptimizerModule.java
@@ -0,0 +1,52 @@
+package pro.gravit.launchermodules.fxruntimeoptimizer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pro.gravit.launcher.base.modules.LauncherInitContext;
+import pro.gravit.launcher.base.modules.LauncherModule;
+import pro.gravit.launcher.base.modules.LauncherModuleInfo;
+import pro.gravit.launcher.base.modules.events.ClosePhase;
+import pro.gravit.launchserver.LaunchServer;
+import pro.gravit.launchserver.binary.tasks.MainBuildTask;
+import pro.gravit.launchserver.modules.events.LaunchServerFullInitEvent;
+import pro.gravit.launchserver.modules.impl.LaunchServerInitContext;
+import pro.gravit.utils.Version;
+
+import java.io.IOException;
+
+public class FxRuntimeOptimizerModule extends LauncherModule {
+    private transient final Logger logger = LogManager.getLogger(FxRuntimeOptimizerModule.class);
+    public static final Version version = new Version(1, 0, 0, 1, Version.Type.STABLE);
+    public RuntimeOptimizerHook hook;
+    private transient MainBuildTask task;
+
+    public FxRuntimeOptimizerModule() {
+        super(new LauncherModuleInfo("FxRuntimeOptimizer", version, new String[]{"LaunchServerCore"}));
+    }
+
+
+    public void finish(LaunchServerFullInitEvent event) {
+        installHooks(event.server);
+    }
+
+    public void installHooks(LaunchServer server) {
+        hook = new RuntimeOptimizerHook(server, this);
+        task = server.launcherBinary.getTaskByClass(MainBuildTask.class).orElseThrow();
+        task.preBuildHook.registerHook(hook);
+    }
+
+    @Override
+    public void init(LauncherInitContext initContext) {
+        registerEvent(this::finish, LaunchServerFullInitEvent.class);
+        registerEvent(this::close, ClosePhase.class);
+        if (initContext instanceof LaunchServerInitContext ctx) {
+            installHooks(ctx.server);
+        }
+    }
+
+    public void close(ClosePhase closePhase) {
+        if(task != null && hook != null) {
+            task.preBuildHook.unregisterHook(hook);
+        }
+    }
+}
---
+++ FxRuntimeOptimizer_module/src/main/java/pro/gravit/launchermodules/fxruntimeoptimizer/RuntimeOptimizerHook.java
@@ -0,0 +1,65 @@
+package pro.gravit.launchermodules.fxruntimeoptimizer;
+
+import javafx.css.Stylesheet;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pro.gravit.launchserver.LaunchServer;
+import pro.gravit.launchserver.binary.BuildContext;
+import pro.gravit.launchserver.binary.tasks.MainBuildTask;
+import pro.gravit.utils.HookException;
+import pro.gravit.utils.helper.IOHelper;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.stream.Stream;
+
+public class RuntimeOptimizerHook implements MainBuildTask.IOHookSet.IOHook<BuildContext> {
+    private transient final Logger logger = LogManager.getLogger(RuntimeOptimizerHook.class);
+    private transient final LaunchServer server;
+    private transient final FxRuntimeOptimizerModule module;
+
+    public RuntimeOptimizerHook(LaunchServer server, FxRuntimeOptimizerModule module) {
+        this.server = server;
+        this.module = module;
+    }
+
+    @Override
+    public void hook(BuildContext context) throws HookException, IOException {
+        var currentRuntimeDir = context.getRuntimeDir();
+        var tmpRuntimeDir = server.tmpDir.resolve("runtime-optimizer");
+        context.setRuntimeDir(tmpRuntimeDir);
+        context.setDeleteRuntimeDir(true);
+        IOHelper.walk(currentRuntimeDir, new OptimizerVisitor(context, currentRuntimeDir, tmpRuntimeDir), true);
+    }
+
+    private class OptimizerVisitor extends SimpleFileVisitor<Path> {
+        private final BuildContext context;
+        private final Path baseDir;
+        private final Path targetDir;
+
+        private OptimizerVisitor(BuildContext context, Path baseDir, Path targetDir) {
+            this.context = context;
+            this.baseDir = baseDir;
+            this.targetDir = targetDir;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Path rel = baseDir.relativize(file);
+            String fileName = rel.getFileName().toString();
+            if(fileName.endsWith(".css")) {
+                fileName = fileName.substring(0, fileName.length()-".css".length()).concat(".bss");
+                Path target = targetDir.resolve(rel.getParent()).resolve(fileName);
+                logger.trace("Convert {} to {}", file, target);
+                IOHelper.createParentDirs(target);
+                Stylesheet.convertToBinary(file.toFile(), target.toFile());
+            } else {
+                Path target = targetDir.resolve(rel);
+                IOHelper.createParentDirs(target);
+                IOHelper.copy(file, target);
+            }
+            return super.visitFile(file, attrs);
+        }
+    }
+}
