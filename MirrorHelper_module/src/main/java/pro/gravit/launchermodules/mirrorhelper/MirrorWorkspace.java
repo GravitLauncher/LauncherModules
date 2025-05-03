b0de6ae44d247c372da93aad3358127ba656b05f
Gravita <12893402+gravit0@users.noreply.github.com>
2025-05-03 22:31:04 +0700

    [FEATURE] Support multiple client types

--- MirrorHelper_module/src/main/java/pro/gravit/launchermodules/mirrorhelper/MirrorWorkspace.java
+++ MirrorHelper_module/src/main/java/pro/gravit/launchermodules/mirrorhelper/MirrorWorkspace.java
@@ -8,9 +8,9 @@ import java.util.Map;
 public record MirrorWorkspace(List<String> fabricMods, List<String> quiltMods, List<String> forgeMods, String lwjgl3version, List<LwjglVersions> lwjglVersionOverride, String fabricLoaderVersion, Map<String, MultiMod> multiMods, List<Library> libraries, Map<String, BuildScript> build) {
     public record Library(String path, String url, String data, Map<String, String> unpack, List<String> prefixFilter) {
     }
-    public record MultiMod(ClientProfile.Version minVersion, ClientProfile.Version maxVersion, InstallClient.VersionType type, String url, String target) {
+    public record MultiMod(ClientProfile.Version minVersion, ClientProfile.Version maxVersion, List<InstallClient.VersionType> clientType, String url, String target) {
         boolean check(InstallClient.VersionType type, ClientProfile.Version version) {
-            if(this.type != null && this.type != type) {
+            if(this.clientType != null && !this.clientType.isEmpty() && this.clientType.contains(type)) {
                 return false;
             }
             if(this.minVersion != null && version.compareTo(this.minVersion) < 0) {
@@ -23,9 +23,9 @@ public record MirrorWorkspace(List<String> fabricMods, List<String> quiltMods, L
         }
     }
 
-    public record BuildScript(List<BuildCommand> script, String result, String path, InstallClient.VersionType type, ClientProfile.Version minVersion, ClientProfile.Version maxVersion, boolean dynamic) {
+    public record BuildScript(List<BuildCommand> script, String result, String path, List<InstallClient.VersionType> clientType, ClientProfile.Version minVersion, ClientProfile.Version maxVersion, boolean dynamic) {
         boolean check(InstallClient.VersionType type, ClientProfile.Version version) {
-            if(this.type != null && this.type != type) {
+            if(this.clientType != null && !this.clientType.isEmpty() && this.clientType.contains(type)) {
                 return false;
             }
             if(this.minVersion != null && version.compareTo(this.minVersion) < 0) {
